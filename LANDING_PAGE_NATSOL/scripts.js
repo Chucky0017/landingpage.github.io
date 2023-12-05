3c8254c6b3bb7e695438ac3c930a282edc88175b
Chucky0017 <149279546+Chucky0017@users.noreply.github.com>
2023-12-05 11:10:22 -0500

    Add files via upload

---
+++ LANDING_PAGE_NATSOL/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('select').formSelect();
+    $('.slider').slider();
+
+    emailjs.init('LHpmZkSOI9G6AM_bg');
+
+    var formMsg = $('#formMsg');
+
+    formMsg.on('submit', function (e) {
+        e.preventDefault();
+
+        let nombre = $('#inputName').val();
+        let apellido = $('#inputLastName').val();
+        let email = $('#inputEmail').val();
+        let nroPais = $('#inputCountry').val();
+        let telefono = $('#inputNumberPhone').val();
+        let mensaje = $('#txaMessage').val();
+
+        enviarCorreo(nombre, apellido, email, nroPais, telefono, mensaje);
+
+        this.reset();
+    });
+
+    function enviarCorreo(nombre, apellido, email, nroPais, telefono, mensaje) {
+        var templateParams = {
+            to_name: `${nombre} ${apellido}`,
+            from_name: "Natsol",
+            user_email: email,
+            user_phone: `${nroPais} ${telefono}`,
+            user_message: mensaje
+        };
+
+        const serviceID = 'default_service';
+        const templateID = 'template_ni08eml';
+
+        emailjs.send(serviceID, templateID, templateParams)
+            .then(function (response) {
+                console.log("Correo enviado con éxito", response);
+
+                M.toast({ html: 'Correo enviado con éxito', classes: 'rounded' });
+            }, function (error) {
+                console.log("Error al enviar el correo", error);
+            });
+    }
+});
